9e258346ec8ee8a3871fc0acbabbf46cff0af278
Rohan Doshi <rohan.doshi02@gmail.com>
2021-07-08 00:42:55 +0530

    Added Question Bank
    as per Vyas Ma'am instructions

--- lib/units/unit1/Content_Data/ListViewUnit1.dart
+++ lib/units/unit1/Content_Data/ListViewUnit1.dart
@@ -1,8 +1,9 @@
 import 'package:engg_chemistry_study_assist/units/unit1/Quiz_2.0/QuizScreen1.dart';
 
 import '../studymaterial.dart';
-import '../QuestionBanks/QuestionBankUnit1.dart';
+// import '../QuestionBanks/QuestionBankUnit1.dart';
 import '../Numerical_Solver/NumericalSolver.dart';
+import '../QuestionBanks/QuestionBankCard.dart';
 
 class ListViewUnit1 {
   int id;
@@ -21,6 +22,6 @@ List content = [
 List funcList = [
   StudyMaterialUnit1(),
   NumericalSolver(),
-  QuestionBankUnit1(),
+  QuestionBankCard(),
   QuizScreen1(),
 ];
