699d841a92ce6d304e3f3ba0c3a1aa3598acdcfd
Rohan Doshi <rohan.doshi02@gmail.com>
2021-07-07 10:45:23 +0530

    Merge code to main branch afterwards

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ Future<void> main() async {
   await Firebase.initializeApp();
   runApp(MyApp());
 }
-
+//todo Merge code to main branch
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
